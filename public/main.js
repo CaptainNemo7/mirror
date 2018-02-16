f8ed95cda1d5d4ab498b6d76a4df9980ea090884
Carter Chavez <Carter.chavez17@gmail.com>
2018-02-15 20:49:10 -0800

    fix video, deprecated code

--- public/main.js
+++ public/main.js
@@ -1,49 +1,47 @@
 const video = document.querySelector('video');
 
+// navigator.mediaDevices.getUserMedia({audio: false, video: true})
+// .then((mediaStream) => {
+// 	// console.log(mediaStream)
+// 	video.src = mediaStream;
+// 	video.onloadedmetadata = e => {
+// 		console.log(e)
+// 		video.play();
+// 	};
+// })
+// .catch((error) => {
+// 	console.log('error getting access: ' + error.name + ' : ' + error.message);
+// })
 
-navigator.mediaDevices.getUserMedia({audio: false, video: true})
-.then((mediaStream) => {
-	console.log(mediaStream)
-	video.src = mediaStream;
-	video.onloadedmetadata = e => {
-		video.play();
-	};
-})
-.catch((error) => {
-	console.log('error getting access: ' + error.name + ' : ' + error.message);
-})
+// being deprecated, need to update
 
+navigator.getMedia = (	navigator.getUserMedia || 
+												navigator.webkitGetUserMedia ||
+												navigator.mozGetUserMedia ||
+												navigator.msGetUserMedia
+											);
+window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
+if ( navigator.getMedia ) {
+	navigator.getMedia (
+		{
+			video: true,
+			audio: false
+		},
 
-
-
-// navigator.getMedia = (	navigator.getUserMedia || 
-// 												navigator.webkitGetUserMedia ||
-// 												navigator.mozGetUserMedia ||
-// 												navigator.msGetUserMedia
-// 											);
-// window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
-
-// if ( navigator.getMedia ) {
-// 	navigator.getMedia (
-// 		{
-// 			video: true,
-// 			audio: false
-// 		},
-
-// 		function(videoStream) {
-// 			video.src = ( window.URL && window.URL.createObjectURL(videoStream)) || videoStream;
-// 			video.play();
-// 		},
+		function(videoStream) {
+			video.src = ( window.URL && window.URL.createObjectURL(videoStream)) || videoStream;
+			video.play();
+		},
 
 
-// 		function(err) {
-// 			console.log('getUserMedia error: ' + err.code);
-// 		}
+		function(err) {
+			console.log('getUserMedia error: ' + err.code);
+		}
 
-// 	);
-// } else {
-// 	console.log('getUserMedia not supported');
-// }
+	);
+} else {
+	console.log('getUserMedia not supported');
+}
 
 

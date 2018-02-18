7afc8192a01d3157990263c9ad8b74e3db76d3a4
Carter Chavez <Carter.chavez17@gmail.com>
2018-02-17 23:05:56 -0800

    tpye of camera

--- public/main.js
+++ public/main.js
@@ -1,11 +1,8 @@
 const video = document.querySelector('video');
-// { 
-// 									width: { min: 1024, ideal: 1280, max: 1920 },
-// 								  height: { min: 776, ideal: 720, max: 1080 } 
-// 								 } 
+
 let constraints = {audio: false, video: {
-																	width: {min: 1280},
-																	height: {min: 720}
+																	width: {ideal: 1280},
+								  								height: {ideal: 720} 
 																}
 								};
 
